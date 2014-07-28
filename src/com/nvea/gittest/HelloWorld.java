20bef3713d59105f7b0664713c50473e9ef6c0ec
razvan <razvan.veina@gmail.com>
2014-07-28 10:50:22 +0300

    sdfasdfasd

--- src/com/nvea/gittest/HelloWorld.java
+++ src/com/nvea/gittest/HelloWorld.java
@@ -1,8 +1,10 @@
+
 package com.nvea.gittest;
 
 public class HelloWorld {
-	public static void main(String[] args) {
-		System.out.println("hello world");
-		System.out.println("hello world brenci 2 3  4");
-	}
+  public static void main(String[] args) {
+    System.out.println("hello world");
+    System.out.println("hello world brenci 2 3  4");
+    System.out.println("work");
+  }
 }
